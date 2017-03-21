a02eda0539f39a8a45bf23f6dcc8e9273c3bc000
gong023 <gon.gong.gone@gmail.com>
2017-03-21 02:19:51 -0700

    add new

--- ayaml.go
+++ ayaml.go
@@ -1,19 +1,38 @@
 package ayaml
 
+import (
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+)
+
 type Marshaler interface {
 	Marshal(v interface{}) ([]byte, error)
 	Unmarshal(data []byte, v interface{}) error
 }
 
+type fixtureFile map[string]map[string]interface{}
+
 type Ayaml struct {
-	Data map[string]interface{}
+	Data *fixtureFile
 }
 
-func (a *Ayaml) RegisterBasePath(path string) {
+func New(filename string) (ayaml Ayaml, err error) {
+	b, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return
+	}
+
+	var f fixtureFile
+	err = yaml.Unmarshal(b, &f)
+	if err != nil {
+		return
+	}
+	ayaml.Data = &f
+	return
 }
 
-func (a Ayaml) Scheme(s string) Ayaml {
-	return a
+func (a Ayaml) Scheme(s string) *Ayaml {
+	return &a
 }
 
 func (a *Ayaml) With(data map[string]interface{}) *Ayaml {
--- ayaml_test.go
+++ ayaml_test.go
@@ -1,7 +1,22 @@
 package ayaml
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
 
-func TestAyaml_RegisterBasePath(t *testing.T) {
-	t.Skip()
+func TestAyamlNew(t *testing.T) {
+	_, err := New("fileNotExists")
+	require.Error(t, err)
+
+	y, err := New("./test_fixture/invalid.yml")
+	require.Error(t, err)
+
+	y, err = New("./test_fixture/a.yml")
+	require.NoError(t, err)
+	m := *y.Data
+	assert.Equal(t, 100000000, m["valid_user"]["id"])
+	assert.Equal(t, "Taro", m["valid_user"]["name"])
+	assert.Equal(t, "2014-01-01 00:00:00", m["valid_user"]["created"])
 }
